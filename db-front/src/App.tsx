b9a68649612ec5711c253c5b1cd5e932156c74b7
PauloUbirajara <ubirajara3@hotmail.com>
2022-02-24 11:35:38 -0300

    create App component

---
+++ db-front/src/App.tsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import SettingsComponent from './components/Settings/SettingsComponent'
+
+
+const App = () => {
+  const [pageSize, setPageSize] = useState(0)
+
+
+  const updatePageSize = (value: number) => {
+    setPageSize(value)
+    console.log(pageSize)
+  }
+
+
+  return <>
+    <div className="main">
+      <SettingsComponent updatePageSize={updatePageSize} />
+    </div>
+  </>
+}
+
+
+export default App
