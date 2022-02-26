e32ee9d9f83a486fd20fdfbd8c7e0985959a67d9
PauloUbirajara <ubirajara3@hotmail.com>
2022-02-26 18:14:34 -0300

    create temporary html in Database component

--- db-front/src/components/Database/DatabaseComponent.tsx
+++ db-front/src/components/Database/DatabaseComponent.tsx
@@ -1,17 +1,46 @@
 import { useParams } from 'react-router-dom'
+import styled from 'styled-components'
+
+
+import Button from '../Button/ButtonComponent'
+
+
+const DatabaseContainer = styled.div`
+    margin: 30px;
+    padding: 25px;
+
+    display: flex;
+    flex-direction: column;
+    align-items: stretch;
+    gap: 10px;
+
+    background: var(--primary-color);
+    color: var(--foreground-color);
+    border-radius: var(--border-radius);
+  `
 
 
 const DatabaseComponent = () => {
   const { pageSize } = useParams()
+
   return <>
-    <div className="database">
+    <DatabaseContainer>
       <h1>Banco de dados</h1>
-      <h2>{pageSize}</h2>
-      <div className="database-pages"></div>
+
+      <h2>Tamanho individual de página: {pageSize}</h2>
+
       <label htmlFor="key-input">Buscar chave</label>
-      <input type="text" autoComplete="false" name="key-input" />
-    </div>
+      <input
+        type="text"
+        placeholder="Digite algum registro do banco"
+        autoComplete="false"
+        name="key-input"
+      />
+      <Button text="Pesquisar" onClick={null} />
+
+    </DatabaseContainer>
   </>
 }
+
 
 export default DatabaseComponent
