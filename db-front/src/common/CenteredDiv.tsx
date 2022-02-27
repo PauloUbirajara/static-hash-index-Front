b7f88c8fcdccd84217d21b0768d4bea9aa330db2
PauloUbirajara <ubirajara3@hotmail.com>
2022-02-27 00:01:17 -0300

    create CenteredDiv component

---
+++ db-front/src/common/CenteredDiv.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+
+const CenteredDiv = styled.div`
+  display: flex;
+  min-height: 80vh;
+  justify-content: center;
+  align-items: center;
+`
+
+export default CenteredDiv
