c69e45bd02a41cb08abdb16eedc93f6df954524a
PauloUbirajara <ubirajara3@hotmail.com>
2022-02-27 00:01:46 -0300

    move Settings component to 'pages' folder

---
+++ db-front/src/pages/SettingsPage.tsx
@@ -0,0 +1,55 @@
+import { useState } from 'react'
+import styled from 'styled-components'
+
+
+import MainTemplate from '../common/MainTemplate'
+import CenteredDiv from '../common/CenteredDiv'
+import { ButtonComponent } from '../components/Button/ButtonComponent'
+import { IconEnum } from '../components/Icon/IconComponent'
+
+
+const Settings = styled.div`
+  display: grid;
+  grid-template-rows: repeat(3, 1fr);
+  gap: 20px;
+
+  padding: 25px;
+
+  background: var(--primary-color);
+  color: var(--foreground-color);
+  border-radius: var(--border-radius);
+`
+
+
+const SettingsPage = () => {
+  const [pageSize, setPageSize] = useState(1)
+
+  const onChange = (el: any) => {
+    setPageSize(+el.target.value)
+  }
+
+  return (
+    <MainTemplate>
+      <CenteredDiv>
+        <Settings>
+          <h1>Configurar tamanho individual da página:</h1>
+          <input
+            type="number"
+            placeholder='Digite um número'
+            min='1'
+            defaultValue={pageSize}
+            onChange={onChange}
+          />
+          <ButtonComponent
+            icon={IconEnum.OPEN_DATABASE}
+            text="Ver banco de dados"
+            to={`/database/${pageSize}`}
+          />
+        </Settings>
+      </CenteredDiv>
+    </MainTemplate>
+  )
+}
+
+
+export default SettingsPage
