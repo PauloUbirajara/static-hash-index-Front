9526b562b4791429f0669005038c079e312496e3
PauloUbirajara <ubirajara3@hotmail.com>
2022-02-27 00:02:35 -0300

    move Database component to 'pages' folder

--- db-front/src/pages/DatabasePage.tsx
+++ db-front/src/pages/DatabasePage.tsx
@@ -1,10 +1,36 @@
-import React from 'react'
-import MainTemplate from '../common/MainTemplate'
+import { useParams } from 'react-router-dom'
 
-export const DatabasePage: React.FC = () => {
-    return (
-        <MainTemplate>
-            <h1>Database Page, the page that will show the pages</h1>
-        </MainTemplate>
-    )
+
+import MainTemplate from '../common/MainTemplate'
+import { ButtonComponent } from '../components/Button/ButtonComponent'
+import { IconEnum } from '../components/Icon/IconComponent'
+
+
+const DatabasePage = () => {
+  const { pageSize } = useParams()
+
+
+  return <MainTemplate>
+    <div className="database">
+      <h1>Banco de dados</h1>
+
+      <h2>Tamanho individual de página: {pageSize || 1}</h2>
+
+      <label htmlFor="key-input">Buscar chave</label>
+      <input
+        type="text"
+        placeholder="Digite algum registro do banco"
+        autoComplete="false"
+        name="key-input"
+      />
+      <ButtonComponent
+        icon={IconEnum.SEARCH}
+        text="Pesquisar"
+        onClick={null}
+      />
+    </div>
+  </MainTemplate>
 }
+
+
+export default DatabasePage
