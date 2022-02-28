695fdb106301289d1df914305f288ee5444fbc95
PauloUbirajara <ubirajara3@hotmail.com>
2022-02-28 20:52:12 -0300

    add basic styling to stats page

--- db-front/src/pages/StatsPage.tsx
+++ db-front/src/pages/StatsPage.tsx
@@ -1,10 +1,19 @@
 import MainTemplate from '../common/MainTemplate'
 
 
-export const StatsPage = () => {
-    return (
-        <MainTemplate>
-            <h1>Stats Page</h1>
-        </MainTemplate>
-    )
+type StatsProps = {
+  pageSize: number;
+}
+
+export const StatsPage = (props: StatsProps) => {
+  const { pageSize } = props;
+
+  return (
+    <MainTemplate>
+      <div className="stats">
+        <h1>Estatísticas sobre banco de dados</h1>
+        <h1>Tamanho individual da página: {pageSize}</h1>
+      </div>
+    </MainTemplate>
+  )
 }
