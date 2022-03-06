119c3e6f4daea300681a2c46b88a795cea38acf8
Paulo Ubirajara de Mattos Neto <ubirajara3@hotmail.com>
2022-03-06 00:38:05 -0300

    indent components

--- db-front/src/App.tsx
+++ db-front/src/App.tsx
@@ -1,23 +1,31 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
 
-
-import { SettingsPage, DatabasePage, StatsPage } from './pages'
-
+import { SettingsPage, DatabasePage, StatsPage } from './pages';
 
 const App = () => {
-  const [pageSize, setPageSize] = useState(1)
+  const [pageSize, setPageSize] = useState(1);
 
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<SettingsPage pageSize={pageSize} updatePageSize={setPageSize} />} />
-        <Route path='/database' element={<DatabasePage pageSize={pageSize} />} />
-        <Route path='/stats' element={<StatsPage pageSize={pageSize} />} />
+        <Route
+          path="/"
+          element={
+            <SettingsPage pageSize={pageSize} updatePageSize={setPageSize} />
+          }
+        />
+        <Route
+          path="/database"
+          element={<DatabasePage pageSize={pageSize} />}
+        />
+        <Route
+          path="/stats"
+          element={<StatsPage pageSize={pageSize} />}
+        />
       </Routes>
     </Router>
-  )
-}
+  );
+};
 
-
-export default App
+export default App;
--- db-front/src/common/CenteredDiv.tsx
+++ db-front/src/common/CenteredDiv.tsx
@@ -1,12 +1,9 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 
-
-const CenteredDiv = styled.div`
+export const CenteredDiv = styled.div`
   display: flex;
   flex-direction: column;
   min-height: 80vh;
   justify-content: center;
   align-items: center;
-`
-
-export default CenteredDiv
+`;
